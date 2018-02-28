ac7763da38dc96e5630e30aa7ac8cef54897a363
randall ishino <rishino24@gmail.com>
2018-02-27 19:26:38 -0800

    created routes to both html pages

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,7 +2,7 @@
 // array to hold user data from survey
 var friends = [{
     name: "Randall Ishino",
-    picture: "http://i.cdn.turner.com/drp/nba/lakers/sites/default/files/styles/logo/public/lakers_logo_500_0_0.png?itok=cOsSNS5C",
+    image: "http://i.cdn.turner.com/drp/nba/lakers/sites/default/files/styles/logo/public/lakers_logo_500_0_0.png?itok=cOsSNS5C",
     scores: [
         2,
         3,
