fb8222ae2422b75bae45a6be9d04a447a9baedd5
randall ishino <rishino24@gmail.com>
2018-02-27 18:17:53 -0800

    friends array dummy data

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+
+// array to hold user data from survey
+var friends = [{
+    name: "Randall Ishino",
+    picture: "http://i.cdn.turner.com/drp/nba/lakers/sites/default/files/styles/logo/public/lakers_logo_500_0_0.png?itok=cOsSNS5C",
+    scores: [
+        2,
+        3,
+        4,
+        1,
+        2,
+        3,
+        1,
+        1,
+        2,
+        4
+    ],
+}];
+
+console.log(friends);
--- server.js
+++ server.js
@@ -2,10 +2,17 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
-var app = express();
 
+// setting up express
+var app = express();
 var PORT = 3000;
 
 // sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+
+// connection listening
+app.listen(PORT, function() {
+    console.log('Friend Finder app is listening on PORT: ' + PORT);
+  });
